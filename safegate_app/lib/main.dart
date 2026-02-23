7d995d8d231376c7ccf1b9e5e228a5eac7cc5036
AnanyaCherukuri05 <ananyacherukuri5@gmail.com>
2026-02-23 13:46:07 +0530

    done

--- safegate_app/lib/main.dart
+++ safegate_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,60 +17,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const WelcomePage(),
-    );
-  }
-}
-
-class WelcomePage extends StatefulWidget {
-  const WelcomePage({super.key});
-
-  @override
-  State<WelcomePage> createState() => _WelcomePageState();
-}
-
-class _WelcomePageState extends State<WelcomePage> {
-  bool _isWelcomed = false;
-
-  void _toggleWelcome() {
-    setState(() {
-      _isWelcomed = !_isWelcomed;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome to SafeGate'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome to SafeGate App!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Icon(
-              _isWelcomed ? Icons.check_circle : Icons.home,
-              size: 100,
-              color: _isWelcomed ? Colors.green : Colors.blue,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              _isWelcomed ? 'You are welcomed!' : 'Tap the button to get started.',
-              style: TextStyle(fontSize: 18, color: _isWelcomed ? Colors.green : Colors.black),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: _toggleWelcome,
-              child: Text(_isWelcomed ? 'Reset' : 'Welcome Me'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ safegate_app/lib/screens/welcome_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  bool _isWelcomed = false;
+
+  void _toggleWelcome() {
+    setState(() {
+      _isWelcomed = !_isWelcomed;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to SafeGate'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to SafeGate App!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Icon(
+              _isWelcomed ? Icons.check_circle : Icons.home,
+              size: 100,
+              color: _isWelcomed ? Colors.green : Colors.blue,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              _isWelcomed ? 'You are welcomed!' : 'Tap the button to get started.',
+              style: TextStyle(fontSize: 18, color: _isWelcomed ? Colors.green : Colors.black),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: _toggleWelcome,
+              child: Text(_isWelcomed ? 'Reset' : 'Welcome Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
